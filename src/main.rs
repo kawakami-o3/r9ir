94876a31e23e91d96195c97ceccbae2c24f197c3
Hayato MIKAMI <mikamihayato383@gmail.com>
2016-12-02 11:09:54 +0900

    implementing string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 use std::io;
+use std::io::Write;
+
+const BUFLEN: i32 = 256;
+
+macro_rules! error {
+  ($fmt:expr) => (writeln!(&mut std::io::stderr(), $fmt));
+  ($fmt:expr,$($x:tt)*) => (writeln!(&mut std::io::stderr(), $fmt, $( $x )*));
+}
+
+
+fn compile_number(n: i32) {
+}
 
 fn main() {
   let mut input = String::new();
@@ -8,11 +20,13 @@ fn main() {
 
   let val = input.trim();
 
+  error!("{}{}\n", val, val);
 
   println!("\t.text\n\t\
             .global _mymain\n\
             _mymain:\n\t\
             mov ${}, %eax\n\t\
             ret\n", val);
+
 }
 
