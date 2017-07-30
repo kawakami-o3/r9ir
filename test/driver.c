f84948751be91effbeed546f14af88c0274b5032
Hayato MIKAMI <mikamihayato383@gmail.com>
2017-07-30 22:32:03 +0900

    add "variable" (wip)
    T/O

--- test/driver.c
+++ test/driver.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
-#define WEAK __attribute__((weak))
-extern int intfn(void) WEAK;
-extern char *stringfn(void) WEAK;
-
 int main(int argc, char **argv) {
-  if (intfn) {
-    printf("%d\n", intfn());
-  } else if (stringfn) {
-    printf("%s\n", stringfn());
-  } else {
-    printf("Should not happen");
-  }
+  printf("%d\n", mymain());
   return 0;
 }
