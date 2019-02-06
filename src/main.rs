b9ad3f2c1126b7ef7aefdcd9a12bc4052004a87a
kawakami <kawakami.ozone@gmail.com>
2019-02-06 09:01:58 +0000

    Add intermediate representation.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,30 @@
 extern crate lazy_static;
 
 use std::boxed::Box;
-use std::cell::Cell;
 use std::env;
 use std::sync::Mutex;
+
+/*
+fn to_node_type(token_type: TokenType) -> NodeType {
+    match token_type {
+        TokenType::ADD => NodeType::ADD,
+        TokenType::SUB => NodeType::SUB,
+        i => {
+            panic!();
+        }
+    }
+}
+*/
+
+fn to_ir_type(node_type: NodeType) -> IRType {
+    match node_type {
+        NodeType::ADD => IRType::ADD,
+        NodeType::SUB => IRType::SUB,
+        _ => {
+            panic!();
+        },
+    }
+}
 
 lazy_static! {
     static ref TOKENS: Mutex<Vec<Token>> = Mutex::new(Vec::new());
@@ -15,8 +36,8 @@ lazy_static! {
 #[derive(Debug, PartialEq)]
 enum TokenType {
     NUM,
-    PLUS,
-    MINUS,
+    ADD,
+    SUB,
     EOF,
 }
 
@@ -48,8 +69,8 @@ fn tokenize(p: &String) {
     for s in tokens {
         if s == "+" || s == "-" {
             let ty = match s.as_str() {
-                "+" => TokenType::PLUS,
-                "-" => TokenType::MINUS,
+                "+" => TokenType::ADD,
+                "-" => TokenType::SUB,
                 _ => panic!(),
             };
             let tok = Token {
@@ -87,8 +108,8 @@ lazy_static! {
 #[derive(Clone, Debug, PartialEq)]
 enum NodeType {
     NUM,
-    PLUS,
-    MINUS,
+    ADD,
+    SUB,
 }
 
 #[derive(Debug)]
@@ -96,7 +117,7 @@ struct Node {
     ty: NodeType,
     lhs: Option<Box<Node>>,
     rhs: Option<Box<Node>>,
-    val: i32,
+    val: usize,
 }
 
 fn new_node(ty: NodeType, lhs: Node, rhs: Node) -> Node {
@@ -108,7 +129,7 @@ fn new_node(ty: NodeType, lhs: Node, rhs: Node) -> Node {
     }
 }
 
-fn new_node_num(val: i32) -> Node {
+fn new_node_num(val: usize) -> Node {
     Node {
         ty: NodeType::NUM,
         lhs: None,
@@ -122,7 +143,7 @@ fn number(tokens: &Vec<Token>, pos: &mut usize) -> Node {
         if t.ty == TokenType::NUM {
             let val = t.val;
             *pos += 1;
-            return new_node_num(val);
+            return new_node_num(val as usize);
         }
         panic!("number expected, but got {}", t.input);
     }
@@ -135,8 +156,8 @@ fn expr(tokens: &Vec<Token>, pos: &mut usize) -> Node {
         let tok = tokens.get(*pos).unwrap();
         let op = &tok.ty;
         let ty = match op {
-            TokenType::PLUS => NodeType::PLUS,
-            TokenType::MINUS => NodeType::MINUS,
+            TokenType::ADD => NodeType::ADD,
+            TokenType::SUB => NodeType::SUB,
             _ => {
                 break;
             }
@@ -144,7 +165,94 @@ fn expr(tokens: &Vec<Token>, pos: &mut usize) -> Node {
         *pos += 1;
         lhs = new_node(ty, lhs, number(tokens, pos));
     }
+
+    let tok = tokens.get(*pos).unwrap();
+    if tok.ty != TokenType::EOF {
+        panic!("stray token: {}", tok.input);
+    }
     return lhs;
+}
+
+// Intermediate representation
+
+#[derive(Debug, PartialEq)]
+enum IRType {
+    IMM,
+    MOV,
+    RETURN,
+    KILL,
+    ADD,
+    SUB,
+    NOP,
+}
+
+struct IR {
+    op: IRType,
+    lhs: usize,
+    rhs: usize,
+}
+
+fn new_ir(op: IRType, lhs: usize, rhs: usize) -> IR {
+    IR {
+        op: op,
+        lhs: lhs,
+        rhs: rhs,
+    }
+}
+
+lazy_static! {
+    static ref INS: Mutex<Vec<IR>> = Mutex::new(Vec::new());
+    // static ref INP: Mutex<usize> = Mutex::new(0);
+    static ref REGNO: Mutex<usize> = Mutex::new(0);
+}
+
+fn gen_ir_sub(node: Node) -> usize {
+    if node.ty == NodeType::NUM {
+        let mut regno = REGNO.lock().unwrap();
+
+        let r = *regno;
+        (*regno) += 1;
+        match INS.lock() {
+            Ok(mut ins) => {
+                ins.push(new_ir(IRType::IMM, r, node.val as usize));
+            }
+            Err(_) => {
+                panic!("[gen_ir_sub:1] ins lock");
+            }
+        }
+
+        return r;
+    }
+
+    assert!(node.ty == NodeType::ADD || node.ty == NodeType::SUB, "not op");
+
+    let lhs = gen_ir_sub(*node.lhs.unwrap());
+    let rhs = gen_ir_sub(*node.rhs.unwrap());
+
+
+    match INS.lock() {
+        Ok(mut ins) => {
+            ins.push(new_ir(to_ir_type(node.ty), lhs, rhs));
+            ins.push(new_ir(IRType::KILL, rhs, 0));
+        }
+        Err(_) => {
+            panic!("[gen_ir_sub:2] ins lock");
+        }
+    }
+    return lhs
+}
+
+fn gen_ir(node: Node) {
+    let r = gen_ir_sub(node);
+
+    match INS.lock() {
+        Ok(mut ins) => {
+            ins.push(new_ir(IRType::RETURN, r, 0));
+        }
+        Err(_) => {
+            panic!("[gen_ir] ins lock");
+        }
+    }
 }
 
 // Code generator
@@ -159,39 +267,92 @@ lazy_static! {
         String::from("r13"),
         String::from("r14"),
         String::from("r15"),
-        String::from("NULL")
     ]);
-    static ref CUR: Mutex<usize> = Mutex::new(0);
+    static ref USED: Mutex<Vec<bool>> = Mutex::new([false; 8].to_vec());
+    static ref REG_MAP: Mutex<Vec<i32>> = Mutex::new(Vec::new());
 }
 
-fn gen(node: Node, cur: &mut usize) -> String {
-    if node.ty == NodeType::NUM {
-        let regs = REGS.lock().unwrap();
-        let reg = regs.get(*cur).unwrap();
-        *cur += 1;
-        if reg == "NULL" {
-            panic!("register exhausted {}", cur);
+fn alloc(ir_reg: usize) -> usize {
+    let mut reg_map = REG_MAP.lock().unwrap();
+    if let Some(i) = reg_map.get(ir_reg) {
+        if *i != -1 {
+            let r = *i as usize;
+            assert!(USED.lock().unwrap().get(r).expect(&format!("r:{}, i:{}", r, *i)), "");
+            return r;
         }
-        println!("  mov {}, {}", reg, node.val);
-        return reg.clone();
     }
 
-    let lhs_box = node.lhs.unwrap();
-    let rhs_box = node.rhs.unwrap();
-    let dst = gen(*lhs_box, cur);
-    let src = gen(*rhs_box, cur);
+    let regs = REGS.lock().unwrap();
+    let mut used = USED.lock().unwrap();
+    for i in 0..regs.len() {
+        if let Some(u) = used.get(i) {
+            if *u {
+                continue;
+            }
+        }
+        used[i] = true;
+        reg_map[ir_reg] = i as i32;
+        return i;
+    }
+    panic!("register exhausted");
+}
 
-    match node.ty {
-        NodeType::PLUS => {
-            println!("  add {}, {}", dst, src);
-            return dst;
+fn kill(i: i32) {
+    let mut used = USED.lock().unwrap();
+    let r = i as usize;
+    assert!(used[r]);
+    used[r] = false;
+}
+
+fn alloc_regs() {
+    let mut ins = INS.lock().unwrap();
+    for i in 0..ins.len() {
+        let ir = &mut ins[i as usize];
+        match ir.op {
+            IRType::IMM => {
+                ir.lhs = alloc(ir.lhs);
+            }
+            IRType::MOV | IRType::ADD | IRType::SUB => {
+                ir.lhs = alloc(ir.lhs);
+                ir.rhs = alloc(ir.rhs);
+            }
+            IRType::RETURN => {
+                let mut reg_map = REG_MAP.lock().unwrap();
+                kill(reg_map[ir.lhs]);
+            }
+            IRType::KILL => {
+                let mut reg_map = REG_MAP.lock().unwrap();
+                kill(reg_map[ir.lhs]);
+                ir.op = IRType::NOP;
+            }
+            _ => { }
         }
-        NodeType::MINUS => {
-            println!("  sub {}, {}", dst, src);
-            return dst;
-        }
-        _ => {
-            panic!("unknown operator");
+    }
+}
+
+fn gen_x86() {
+    let ins = INS.lock().unwrap();
+    let regs = REGS.lock().unwrap();
+    for i in 0..ins.len() {
+        let ir = &ins[i as usize];
+        match ir.op {
+            IRType::IMM => {
+                println!("  mov {}, {}", regs[ir.lhs as usize], ir.rhs);
+            }
+            IRType::MOV => {
+                println!("  mov {}, {}", regs[ir.lhs as usize], regs[ir.rhs as usize]);
+            }
+            IRType::RETURN => {
+                println!("  mov rax, {}", regs[ir.lhs as usize]);
+                println!("  ret");
+            }
+            IRType::ADD => {
+                println!("  add {}, {}", regs[ir.lhs as usize], regs[ir.rhs as usize]);
+            }
+            IRType::SUB => {
+                println!("  sub {}, {}", regs[ir.lhs as usize], regs[ir.rhs as usize]);
+            }
+            _ => { }
         }
     }
 }
@@ -203,20 +364,29 @@ fn main() {
         return;
     }
 
+    match REG_MAP.lock() {
+        Ok(mut reg_map) => {
+            for _i in 0..1000 {
+                reg_map.push(-1);
+            }
+        }
+        _ => { }
+    }
+
     // Tokenize and parse.
     tokenize(&argv[1]);
     let mut pos = POS.lock().unwrap();
     let tokens = TOKENS.lock().unwrap();
     let node = expr(&tokens, &mut pos);
 
+    gen_ir(node);
+    alloc_regs();
+
     // Print the prologue.
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
 
-    // Generate code while descending the parse tree.
-    let mut cur = CUR.lock().unwrap();
-    println!("  mov rax, {}", gen(node, &mut cur));
-    println!("  ret");
+    gen_x86();
     return;
 }
