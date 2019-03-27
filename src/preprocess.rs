e7c128d2a603e616fd9a112bd6d2c924c61babbf
kawakami <kawakami.ozone@gmail.com>
2019-03-27 23:04:18 +0900

    Split preprocess() into smaller functions.

--- src/preprocess.rs
+++ src/preprocess.rs
@@ -5,97 +5,176 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 
 thread_local! {
-    static DEFINED: RefCell<HashMap<String, Vec<Token>>> = RefCell::new(HashMap::new());
+    static MACROS: RefCell<HashMap<String, Vec<Token>>> = RefCell::new(HashMap::new());
+
+    static CTX: RefCell<Context> = RefCell::new(Context::new());
 }
 
-fn defined_get(key: &String) -> Option<Vec<Token>> {
-    DEFINED.with(|defined| {
-        match defined.borrow().get(key) {
+fn set_ctx(ctx: Context) {
+    CTX.with(|c| {
+        *c.borrow_mut() = ctx;
+    })
+}
+
+fn get_ctx() -> Option<Context> {
+    CTX.with(|c| {
+        Some(c.borrow().clone())
+    })
+}
+
+fn ctx_pop() {
+    CTX.with(|c| {
+        let next = c.borrow().next.clone();
+        if next.is_some() {
+            *c.borrow_mut() = *next.unwrap();
+        }
+    })
+}
+
+fn ctx_output() -> Vec<Token> {
+    CTX.with(|c| {
+        return c.borrow().output.clone();
+    })
+}
+
+fn macros_get(key: &String) -> Option<Vec<Token>> {
+    MACROS.with(|m| {
+        match m.borrow().get(key) {
             Some(v) => Some(v.clone()),
             None => None,
         }
     })
 }
 
-fn defined_put(key: String, value: Vec<Token>) {
-    DEFINED.with(|defined| {
-        defined.borrow_mut().insert(key, value);
+fn macros_put(key: String, value: Vec<Token>) {
+    MACROS.with(|m| {
+        m.borrow_mut().insert(key, value);
     })
 }
 
+#[derive(Clone, Debug)]
+struct Context {
+    input: Vec<Token>,
+    output: Vec<Token>,
+    pos: usize,
+    next: Option<Box<Context>>,
+}
+
+impl Context {
+    fn new() -> Context {
+        Context {
+            input: Vec::new(),
+            output: Vec::new(),
+            pos: 0,
+            next: None,
+        }
+    }
+}
+
+fn new_ctx(next: Option<Context>, input: Vec<Token>) -> Context {
+    let mut ctx = Context::new();
+    ctx.input = input;
+    if next.is_none() {
+        ctx.next = None;
+    } else {
+        ctx.next = Some(Box::new(next.unwrap()));
+    }
+    return ctx;
+}
+
+fn append(v: &mut Vec<Token>) {
+    CTX.with(|ctx| {
+        ctx.borrow_mut().output.append(v);
+    })
+}
+
+fn add(t: Token) {
+    CTX.with(|ctx| {
+        ctx.borrow_mut().output.push(t);
+    })
+}
+
+fn next() -> Token {
+    CTX.with(|ctx| {
+        let pos = ctx.borrow().pos;
+        assert!(pos < ctx.borrow().input.len());
+        ctx.borrow_mut().pos += 1;
+        return ctx.borrow().input[pos].clone();
+    })
+}
+
+fn eof() -> bool {
+    CTX.with(|c| {
+        let ctx = c.borrow();
+        return ctx.pos == ctx.input.len();
+    })
+}
+
+fn get(ty: TokenType, msg: String) -> Token {
+    let t = next();
+    if t.ty != ty {
+        bad_token(&t, msg);
+    }
+    return t;
+}
+
+fn define() {
+    let mut t = get(TokenType::IDENT, "macro name expected".to_string());
+    let name = t.name;
+
+    let mut v = Vec::new();
+    while !eof() {
+        t = next();
+        if t.ty == TokenType::NEW_LINE {
+            break;
+        }
+        v.push(t.clone());
+    }
+    macros_put(name, v);
+}
+
+fn include() {
+    let t = get(TokenType::STR, "string expected".to_string());
+    let path = t.str_cnt;
+    get(TokenType::NEW_LINE, "newline expected".to_string());
+    return append(&mut tokenize(path, false));
+}
+
 pub fn preprocess(tokens: Vec<Token>) -> Vec<Token> {
-    // if(!defined) ...
+    set_ctx(new_ctx(get_ctx(), tokens.clone()));
 
-    let mut v: Vec<Token> = Vec::new();
-
-    let mut i = 0;
-    while i < tokens.len() {
-        let mut t = &tokens[i];
-        i += 1;
+    while !eof() {
+        let mut t = next();
 
         if t.ty == TokenType::IDENT {
-            match defined_get(&t.name) {
+            match macros_get(&t.name) {
                 Some(macro_token) => {
-                    v.append(&mut macro_token.clone());
+                    append(&mut macro_token.clone());
                 }
                 None => {
-                    v.push(t.clone());
+                    add(t.clone());
                 }
             }
             continue;
         }
 
         if t.ty != TokenType::SHARP {
-            v.push(t.clone());
+            add(t.clone());
             continue;
         }
 
-        t = &tokens[i];
-        i += 1;
-        if t.ty != TokenType::IDENT {
-            bad_token(&t, "identifier expected".to_string());
-        }
+        t = get(TokenType::IDENT, "identifier expected".to_string());
 
         if t.name == "define" {
-            t = &tokens[i];
-            i += 1;
-            if t.ty != TokenType::IDENT {
-                bad_token(&t, "macro name expected".to_string());
-            }
-            let name = t.name.clone();
-
-            let mut v2 = Vec::new();
-            while i < tokens.len() {
-                t = &tokens[i];
-                i += 1;
-                if t.ty == TokenType::NEW_LINE {
-                    break;
-                }
-                v2.push(t.clone());
-            }
-            defined_put(name, v2);
-            continue;
-        }
-
-        if t.name == "include" {
-            t = &tokens[i];
-            i += 1;
-            if t.ty != TokenType::STR {
-                bad_token(&t, "string expected".to_string());
-            }
-
-            let path = t.str_cnt.clone();
-
-            t = &tokens[i];
-            i += 1;
-            if t.ty != TokenType::NEW_LINE {
-                bad_token(&t, "newline expected".to_string());
-            }
-
-            v.append(&mut tokenize(path, false));
-            continue;
+            define();
+        } else if t.name == "include" {
+            include();
+        } else {
+            bad_token(&t, "unknown directive".to_string());
         }
     }
 
+    let v = ctx_output();
+    ctx_pop();
     return v;
 }
