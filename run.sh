bfc1d5c6ea85fba9a01e080ffb2b7cd9b0bd32b5
kawakami <kawakami.ozone@gmail.com>
2019-06-15 23:27:40 +0900

    Delete unused scripts.

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#cargo run -q "$(gcc -E -P test/test.c)" 
-
-
-#env RUST_BACKTRACE=1 cargo run "$(gcc -E -P test/orig.c)" 
-env RUST_BACKTRACE=1 cargo run -q "$(gcc -E -P test/orig.c)"  > tmp-test.s
-
-
