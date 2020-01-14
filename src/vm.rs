56d75ed3ca1ac95430c7ea16e0bb57a23f46973a
kawakami <kawakami.ozone@gmail.com>
2020-01-14 16:51:26 +0900

    Add Emulator to vm.

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,15 @@
 
+const REGISTERS_COUNT: usize = 256;
+
+struct Emulator {
+    registers: [u32; REGISTERS_COUNT],
+    eflags: u32,
+    eip: u32,
+}
+
+impl Emulator {
+}
+
 fn main() {
     println!("hello");
 }
