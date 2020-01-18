0a7bf12f5e49e6a79ce992f312e4c7a6dbfb83a9
kawakami <kawakami.ozone@gmail.com>
2020-01-18 17:49:39 +0900

    Update vm.

--- src/vm.rs
+++ src/vm.rs
@@ -45,7 +45,7 @@ impl Emulator {
         regs[ESP.addr()] = esp;
         Emulator {
             registers: regs,
-            memory: Vec::with_capacity(size),
+            memory: vec![0; MEMORY_SIZE],//Vec::with_capacity(size),
             eflags: 0,
             eip: eip,
         }
@@ -63,8 +63,8 @@ impl Emulator {
         self.memory[self.eip as usize + index] as u32
     }
 
-    fn get_sign_code8(&self, index: usize) -> i32 {
-        self.memory[self.eip as usize + index] as i32
+    fn get_sign_code8(&self, index: usize) -> i8 {
+        self.memory[self.eip as usize + index] as i8
     }
 
     fn get_code32(&self, index: usize) -> u32 {
@@ -75,24 +75,34 @@ impl Emulator {
         return ret;
     }
 
-    fn mov_r32_imm32(&self) {
+    fn mov_r32_imm32(&mut self) {
+        let reg = self.get_code8(0) - 0xB8;
+        let value = self.get_code32(1);
+        self.registers[reg as usize] = value;
+        self.eip += 5;
     }
 
-    fn short_jump(&self) {
+    fn short_jump(&mut self) {
+        let diff = self.get_sign_code8(1);
+        self.eip = (self.eip as i32 + diff as i32 + 2) as u32;
     }
 }
 
-fn init_instructions() -> Vec<Box<Fn(&Emulator)>> {
-    let mut v: Vec<Box<Fn(&Emulator)>> = Vec::new();
+fn init_instructions() -> Vec<Option<Box<dyn Fn(&mut Emulator)>>> {
+    //let mut v: Vec<Box<dyn Fn(&Emulator)>> = Vec::new();
+    let mut v: Vec<Option<Box<dyn Fn(&mut Emulator)>>> = Vec::new();
 
     for _ in 0..256 {
-        v.push(Box::new(|_| {}));
+        //v.push(Box::new(|_| {}));
+        v.push(None);
     }
 
     for i in 0..8 {
-        v[0xB8 + i ] = Box::new(Emulator::mov_r32_imm32);
+        //v[0xB8 + i ] = Box::new(Emulator::mov_r32_imm32);
+        v[0xB8 + i ] = Some(Box::new(Emulator::mov_r32_imm32));
     }
-    v[0xEB] = Box::new(Emulator::short_jump);
+    //v[0xEB] = Box::new(Emulator::short_jump);
+    v[0xEB] = Some(Box::new(Emulator::short_jump));
 
     return v;
 }
@@ -107,15 +117,45 @@ fn main() {
     let mut emu = Emulator::create(MEMORY_SIZE, 0x0000, 0x7c00);
 
     let filename: & String = & args[1];
-    let mut buffer = String::new();
 
-    let mut f = File::open(filename).unwrap();
-
-    for byte in f.bytes() {
-        emu.memory.push(byte.unwrap());
+    match File::open(filename) {
+        Ok(f) => {
+            let mut i = 0;
+            for byte in f.bytes() {
+                //emu.memory.push(byte.unwrap());
+                emu.memory[i] = byte.unwrap();
+                i+=1;
+            }
+        }
+        _ => {
+            println!("Failed to open {}.", filename);
+            return;
+        }
     }
 
     let instructions = init_instructions();
 
+    while emu.eip < MEMORY_SIZE as u32 {
+        let code = emu.get_code8(0);
+
+        println!("EIP = {:X}, Code = {:X}", emu.eip, code);
+
+        match &instructions[code as usize] {
+            None => {
+                println!("Not implemented: {}", code);
+                break;
+            }
+            Some(f) => {
+                f(&mut emu);
+            }
+        }
+
+        if emu.eip == 0x00 {
+            println!("\n\nend of program.\n");
+            break;
+        }
+    }
+
+    emu.dump_registers();
 
 }
